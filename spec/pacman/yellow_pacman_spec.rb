64d42d2c8e27c80c9dc68501983d9be9c24382c4
Christine Schorn <cschorn91@gmail.com>
2019-11-13 12:36:26 +1100

    added move methods so pacman can move north, east, south and west

--- spec/pacman/yellow_pacman_spec.rb
+++ spec/pacman/yellow_pacman_spec.rb
@@ -4,9 +4,44 @@ RSpec.describe Pacman::YellowPacman do
   subject { Pacman::YellowPacman.new(0) }
 
   context 'move' do
-    it 'moves 3 spaces' do
-      3.times subject.move
-      expect(subject.position).to eq(3)
+    it 'moves 3 spaces east' do
+      3.times { subject.move_east }
+      expect(subject.east).to eq(3)
+    end
+
+    it 'moves 4 spaces east' do
+      4.times { subject.move_east }
+      expect(subject.east).to eq(4)
+    end
+
+    it 'moves 3 spaces west' do
+      3.times { subject.move_west }
+      expect(subject.east).to eq(-3)
+    end
+
+    it 'moves 4 spaces west' do
+      4.times { subject.move_west }
+      expect(subject.east).to eq(-4)
+    end
+
+    it 'moves 3 spaces north' do
+      3.times { subject.move_north }
+      expect(subject.north).to eq(3)
+    end
+
+    it 'moves 4 spaces north' do
+      4.times { subject.move_north }
+      expect(subject.north).to eq(4)
+    end
+
+    it 'moves 3 spaces south' do
+      3.times { subject.move_south }
+      expect(subject.north).to eq(-3)
+    end
+
+    it 'moves 4 spaces south' do
+      4.times { subject.move_south }
+      expect(subject.north).to eq(-4)
     end
   end
 end
