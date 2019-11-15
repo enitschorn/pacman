7e8a2a17d032d664ca2cb5bd67fa4d6b0b22c789
Christine Schorn <cschorn91@gmail.com>
2019-11-15 12:27:54 +1100

    implemented a run method in the CLI class so commands can be send to be simulator and processed

--- spec/pacman/simulator_spec.rb
+++ spec/pacman/simulator_spec.rb
@@ -3,18 +3,48 @@ require 'spec_helper'
 RSpec.describe Pacman::Simulator do
   let(:grid) { Pacman::Grid.new(5, 5) }
   subject { Pacman::Simulator.new(grid) }
+  
+  context 'before pacman has been placed' do
+    it 'places the pacman onto a valid position' do
+      expect(Pacman::YellowPacman).to receive(:new)
+      .with(0, 0, 'NORTH')
+      .and_return(double)
+      subject.place(0, 0, 'NORTH')
+      expect(subject.pacman).not_to be_nil
+    end
 
-  it 'places the pacman onto a valid position' do
-    expect(Pacman::YellowPacman).to receive(:new)
-    .with(0, 0, 'NORTH')
-    .and_return(double)
-    subject.place(0, 0, 'NORTH')
-    expect(subject.pacman).not_to be_nil
+    it 'cannot place pacman onto an invalid position' do
+      expect(Pacman::YellowPacman).not_to receive(:new)
+      subject.place(5, 5, 'NORTH')
+      expect(subject.pacman).to be_nil
+    end
   end
 
-  it 'cannot place pacman onto an invalid position' do
-    expect(Pacman::YellowPacman).not_to receive(:new)
-    subject.place(5, 5, 'NORTH')
-    expect(subject.pacman).to be_nil
+  context 'when pacman has been placed' do
+    let(:pacman) { instance_double(Pacman::YellowPacman) }
+
+    before do
+      allow(subject).to receive(:pacman).and_return(pacman)
+    end
+
+    it 'tells pacman to move' do
+      expect(pacman).to receive(:move)
+      subject.move
+    end
+
+    it 'tells pacman to turn_left' do
+      expect(pacman).to receive(:turn_left)
+      subject.turn_left
+    end
+
+    it 'tells pacman to turn_right' do
+      expect(pacman).to receive(:turn_right)
+      subject.turn_right
+    end
+
+    it 'tells pacman to report its current position' do
+      expect(pacman).to receive(:report)
+      subject.report
+    end
   end
 end
